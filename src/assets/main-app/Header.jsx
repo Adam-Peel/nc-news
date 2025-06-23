85e0156a52c4d8fc4659b8c670b5475c80882dc3
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-06-23 20:53:31 +0100

    Refactor logo and header and fix scroll on basic css layout

--- src/assets/main-app/Header.jsx
+++ src/assets/main-app/Header.jsx
@@ -2,13 +2,15 @@ function Header() {
   return (
     <header className="header main-header">
       <section id="main-header-logo">
-        <img src="../../../public/nc-logo.png" id="header-hero-image"></img>
+        <img src="/nc-logo.png" id="header-hero-image"></img>
       </section>
-      <section id="main-header-home-button">
-        <button>Button to go home</button>
-      </section>
-      <section id="main-header-login-button">
-        <button>Login</button>
+      <section id="main-header-buttons">
+        <div>
+          <button>Button to go home</button>
+        </div>
+        <div>
+          <button>Login</button>
+        </div>
       </section>
     </header>
   );
