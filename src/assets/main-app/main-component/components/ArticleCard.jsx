d8f8b8ede8d0b00cdd7588b8288eed36f1c7638a
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-06-23 23:51:21 +0100

    format data extraction fron API into card feed

--- src/assets/main-app/main-component/components/ArticleCard.jsx
+++ src/assets/main-app/main-component/components/ArticleCard.jsx
@@ -1,27 +1,30 @@
 function ArticleCard(article) {
+  {
+    console.log(article);
+  }
   return (
-    <section className="article-card" key={article.id}>
+    <section className="article-card" key={article.article.id}>
       <section className="article-card-head">
         <div className="article-card-head-image-placeholder">
           <img
             className="article-card-head-image"
-            src={article.article_img_url}
+            src={article.article.article_img_url}
             alt="An image of the article"
           ></img>
         </div>
         <div className="article-card-head-info">
-          <h2>{article.title}</h2>
-          <p>{article.topic}</p>
+          <h2>{article.article.title}</h2>
+          <p>{article.article.topic}</p>
           <br />
-          {article.author}
+          {article.article.author}
           {" : "}
-          {article.created_at}
+          {article.article.created_at}
         </div>
       </section>
       <section className="article-card-stats">
         <ul className="article-card-stats-list">
-          <li>{article.comment_count}</li>
-          <li>{article.votes}</li>
+          <li>{article.article.comment_count}</li>
+          <li>{article.article.votes}</li>
         </ul>
       </section>
     </section>
