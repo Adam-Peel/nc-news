6f43b855efdb68b16e5538acec14d1fd3feaf6d9
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-06-24 16:28:26 +0100

    Reduce redundant code

--- src/assets/main-app/MainComponent.jsx
+++ src/assets/main-app/MainComponent.jsx
@@ -6,44 +6,21 @@ import UserPage from "./main-component/routes/UserPage";
 import Sidebar from "./main-component/Sidebar";
 import SingleArticlePage from "./main-component/routes/SingleArticlePage";
 import { Routes, Route } from "react-router";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function MainComponent(userId) {
   const [baseURL, setBaseURL] = useState(
     "https://news-aggregator-7e9t.onrender.com/api/articles"
   );
-  const [awaitingAPI, setAwaitingAPI] = useState(true);
-  const [error, setError] = useState(null);
 
   return (
     <>
       <Sidebar userId={userId} setBaseURL={setBaseURL} />
       {/* Routing here for different pages - use API REQUEST*/}
       <Routes>
-        <Route
-          path="/"
-          element={
-            <ArticlesPage
-              awaitingAPI={awaitingAPI}
-              baseURL={baseURL}
-              setAwaitingAPI={setAwaitingAPI}
-              error={error}
-              setError={setError}
-            />
-          }
-        />
+        <Route path="/" element={<ArticlesPage baseURL={baseURL} />} />
         <Route path="/profile" element={<UserPage />} />
-        <Route
-          path="/articles/:article_id"
-          element={
-            <SingleArticlePage
-              awaitingAPI={awaitingAPI}
-              setAwaitingAPI={setAwaitingAPI}
-              error={error}
-              setError={setError}
-            />
-          }
-        />
+        <Route path="/articles/:article_id" element={<SingleArticlePage />} />
       </Routes>
     </>
   );
