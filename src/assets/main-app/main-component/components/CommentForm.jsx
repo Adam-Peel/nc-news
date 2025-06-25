64787ac819c42a7799f5e44aff8a3ac514f9ce78
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-06-25 14:25:08 +0100

    removed body tag to fix hydration

--- src/assets/main-app/main-component/components/CommentForm.jsx
+++ src/assets/main-app/main-component/components/CommentForm.jsx
@@ -4,56 +4,57 @@ import postData from "../utils/post";
 import PostAddIcon from "@mui/icons-material/PostAdd";
 
 function CommentForm({ articleId }) {
-  const [commentValue, setCommentValue] = useState("");
-  const [postError, setPostError] = useState(null);
+  // const [commentValue, setCommentValue] = useState("");
+  // const [postError, setPostError] = useState(null);
 
-  function handleChange(event) {
-    const value = event.target.value;
-    console.log(value);
-    setCommentValue(value);
-  }
+  // function handleChange(event) {
+  //   const value = event.target.value;
+  //   console.log(value);
+  //   setCommentValue(value);
+  // }
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-    const value = event.target.value;
-    console.log(value);
-    const { currentUser } = useContext(UserContext);
-    const destructuredUser = currentUser[0];
-    const url = `https://news-aggregator-7e9t.onrender.com/api/articles/${articleId}/comments`;
-    const postToSend = [
-      { username: destructuredUser.username, body: commentValue },
-    ];
-    try {
-      const post = await postData(url, postToSend);
-      setPostError(null);
-      setCommentValue("");
-    } catch (err) {
-      setPostError("🪧 Sorry, the post did not work 🪧");
-    }
-  }
-  console.log(commentValue);
+  // async function handleSubmit(event) {
+  //   event.preventDefault();
+  //   const value = event.target.value;
+  //   console.log(value);
+  //   const { currentUser } = useContext(UserContext);
+  //   const destructuredUser = currentUser[0];
+  //   const url = `https://news-aggregator-7e9t.onrender.com/api/articles/${articleId}/comments`;
+  //   const postToSend = [
+  //     { username: destructuredUser.username, body: commentValue },
+  //   ];
+  //   try {
+  //     const post = await postData(url, postToSend);
+  //     setPostError(null);
+  //     setCommentValue("");
+  //   } catch (err) {
+  //     setPostError("🪧 Sorry, the post did not work 🪧");
+  //   }
+  // }
+  // console.log(commentValue);
 
   return (
-    <section id="commenting-area">
-      {/* <form method="post" name="comment-form" onSubmit={handleSubmit}> */}
-      <label htmlFor="comment-textarea-input">
-        Add a comment:
-        {/* <br /> */}
-        <textarea
-          id="comment-textarea-input"
-          name="comment-body"
-          // value={commentValue}
-          // onChange={handleChange}
-        />
-      </label>
-      <div id="comment-textarea-button">
-        <button className="user-article-interaction-box-button" type="submit">
-          <PostAddIcon fontSize="small" />
-        </button>
-        <span>{postError}</span>
-      </div>
-      {/* </form> */}
-    </section>
+    <textarea />
+    // <section id="commenting-area">
+    //   {/* <form method="post" name="comment-form" onSubmit={handleSubmit}> */}
+    //   <label htmlFor="comment-textarea-input">
+    //     Add a comment:
+    //     {/* <br /> */}
+    //     <textarea
+    //       id="comment-textarea-input"
+    //       name="comment-body"
+    //       // value={commentValue}
+    //       // onChange={handleChange}
+    //     />
+    //   </label>
+    //   <div id="comment-textarea-button">
+    //     <button className="user-article-interaction-box-button" type="submit">
+    //       <PostAddIcon fontSize="small" />
+    //     </button>
+    //     <span>{postError}</span>
+    //   </div>
+    //   {/* </form> */}
+    // </section>
   );
 }
 
--- src/assets/main-app/main-component/routes/SingleArticlePage.jsx
+++ src/assets/main-app/main-component/routes/SingleArticlePage.jsx
@@ -95,7 +95,7 @@ function SingleArticlePage() {
           articleVotes={articleData.article.votes}
           articleCommentCount={articleData.article.comment_count}
         />
-        {/* BUG -<CommentForm articleId={articleData.article.article_id} /> */}
+        -<CommentForm articleId={articleData.article.article_id} />
         <CommentsFeed articleId={articleData.article.article_id} />
       </article>
     </main>
