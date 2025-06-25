915bfb80702e8a0cd9dd368ad445de0ba2ddb12a
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-06-25 20:04:09 +0100

    Add basic styling for topic pages

--- src/assets/main-app/main-component/routes/ArticlesPage.jsx
+++ src/assets/main-app/main-component/routes/ArticlesPage.jsx
@@ -6,10 +6,15 @@ function ArticlesPage() {
   const { topic } = useParams();
 
   if (topic) {
+    const title = topic[0].toUpperCase();
     return (
       <>
         <main>
           <SearchBar />
+          <h2>
+            <span className="article-start-accent">{title}</span>
+            <span className="article-start-accent">{topic.slice(1)}</span>
+          </h2>
           <ArticlesFeed url={`?topic=${topic}`} />
         </main>
       </>
