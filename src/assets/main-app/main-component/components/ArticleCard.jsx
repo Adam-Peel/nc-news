90a33f5e2cfcfc81410027e7eaa332d2191afbbe
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-06-26 21:10:46 +0100

    Update css styling for articles feed to better allow responsive display

--- src/assets/main-app/main-component/components/ArticleCard.jsx
+++ src/assets/main-app/main-component/components/ArticleCard.jsx
@@ -1,59 +1,73 @@
 import CommentIcon from "@mui/icons-material/Comment";
 import HowToVoteIcon from "@mui/icons-material/HowToVote";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
-import PersonIcon from "@mui/icons-material/Person";
+
 import Badge from "@mui/material/Badge";
 import { useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";
 import provideAvatar from "../utils/provideAvatar";
+// Here
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardActions from "@mui/material/CardActions";
 
 function ArticleCard(article) {
   const { users } = useContext(UserContext);
+
   return (
-    <section className="article-card" key={article.article.id}>
-      <img
-        className="article-card-head-image"
-        src={article.article.article_img_url}
-        alt="An image of the article"
-      ></img>
-      <section>
-        <div className="article-card-head-info">
-          <h3 className="article-card-head-title"> {article.article.title}</h3>
-          <h4 className="article-card-subtitle">{article.article.topic}</h4>
-          <span>
-            {provideAvatar(article.article.author, 30, users)}
-            {article.article.author}
-          </span>
-          <br />
-          <CalendarMonthIcon />
-          {article.article.created_at.slice(0, 10)}
-        </div>
-      </section>
-      <section className="article-card-stats">
-        <ul className="article-card-stats-list">
-          <li key={article.article.id}>
-            <Badge
-              badgeContent={article.article.comment_count}
-              color="primary"
-              showZero
+    <div className="article-card-container" key={article.article.id}>
+      <Card sx={{ width: 350 }} className="article-card">
+        <CardHeader
+          avatar={provideAvatar(article.article.author, 35, users)}
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title={article.article.author}
+          subheader={article.article.created_at.slice(0, 10)}
+        />
+        <CardMedia
+          component="img"
+          height="140"
+          image={article.article.article_img_url}
+          alt="An image of the article"
+        />
+        <CardActionArea>
+          <CardContent>
+            <Typography
+              gutterBottom
+              variant="h6"
+              component="div"
+              textAlign="left"
             >
-              <CommentIcon />
-            </Badge>
-          </li>
-        </ul>
-        <ul className="article-card-stats-list">
-          <li key={article.article.id}>
-            <Badge
-              badgeContent={article.article.votes}
-              color="primary"
-              showZero
-            >
-              <HowToVoteIcon />
-            </Badge>
-          </li>
-        </ul>
-      </section>
-    </section>
+              {article.article.title}
+            </Typography>
+            <Typography variant="body2" sx={{ color: "text.secondary" }}>
+              {article.article.topic}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Badge
+            badgeContent={article.article.comment_count}
+            color="primary"
+            showZero
+          >
+            <CommentIcon />
+          </Badge>
+          <Badge badgeContent={article.article.votes} color="primary" showZero>
+            <HowToVoteIcon />
+          </Badge>
+        </CardActions>
+      </Card>
+    </div>
   );
 }
 
