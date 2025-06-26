aa8fb5ffe14efd6916572b484d634c9a3b663b7e
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-06-27 00:14:12 +0100

    Initial restyle of sidebar for responsive views

--- src/assets/main-app/main-component/Sidebar.jsx
+++ src/assets/main-app/main-component/Sidebar.jsx
@@ -2,103 +2,265 @@ import { useContext, useState } from "react";
 import { UserContext } from "../contexts/UserContext";
 import { useNavigate } from "react-router";
 import Avatar from "@mui/material/Avatar";
-import List from "@mui/material/List";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import Collapse from "@mui/material/Collapse";
-import ExpandLess from "@mui/icons-material/ExpandLess";
-import ExpandMore from "@mui/icons-material/ExpandMore";
 import fetchData from "./utils/fetch";
+
+import { styled } from "@mui/material/styles";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+
+const drawerWidth = 200;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  variants: [
+    {
+      props: ({ open }) => open,
+      style: {
+        // marginLeft: drawerWidth,
+        width: drawerWidth,
+        transition: theme.transitions.create(["width", "margin"], {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+      },
+    },
+  ],
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  variants: [
+    {
+      props: ({ open }) => open,
+      style: {
+        ...openedMixin(theme),
+        "& .MuiDrawer-paper": openedMixin(theme),
+      },
+    },
+    {
+      props: ({ open }) => !open,
+      style: {
+        ...closedMixin(theme),
+        "& .MuiDrawer-paper": closedMixin(theme),
+      },
+    },
+  ],
+}));
 
 function Sidebar() {
-  const { currentUser } = useContext(UserContext);
   const [open, setOpen] = useState(false);
+  const { currentUser } = useContext(UserContext);
   const [topicList, setTopicList] = useState(null);
   const [topicMessage, setTopicMessage] = useState(null);
 
   const handleClick = async function () {
     setOpen(!open);
-    if (open) {
+    if (topicList) {
       return;
     }
-
-    setTopicMessage("loading");
+    setTopicMessage("loading\ntopics");
     try {
       const fetchedTopics = await fetchData(
         `https://news-aggregator-7e9t.onrender.com/api/topics`
       );
       setTopicList(fetchedTopics);
-      setTopicMessage(null);
+      setTopicMessage("Topics:");
     } catch (err) {
       setTopicMessage("Error");
     }
   };
 
   let navigate = useNavigate();
+  console.log(topicList);
 
   return (
     <aside>
-      <div className="sidebar user-avatar">
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleClick}>
+            {!open ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
         <Avatar
           alt={currentUser[0].name}
           src={currentUser[0].avatar_url}
           sx={{ width: 65, height: 65 }}
         />
-      </div>
-      <div id="main-header-stats-div"></div>
-      <nav>
-        <div className="sidebar topic-list">
-          <List
-            sx={{ width: "100%", maxWidth: 200, pl: 0 }}
-            component="nav"
-            aria-labelledby="nested-list-subheader"
-            className="sidebar topic-list"
-          >
-            <ListItemButton sx={{ pl: 0 }}>
-              <ListItemText primary={currentUser[0].name} />
-            </ListItemButton>
-            <ListItemButton sx={{ pl: 0 }}>
-              <ListItemText
-                primary="All Posts"
+        <List component="div" disablePadding>
+          <ListItemButton sx={{ pl: 2 }}></ListItemButton>
+        </List>
+        <List component="div" disablePadding>
+          <ListItem key="all-posts" disablePadding sx={{ display: "block" }}>
+            <ListItemButton
+              sx={[
+                {
+                  minHeight: 48,
+                  px: 2.5,
+                },
+                open
+                  ? {
+                      justifyContent: "initial",
+                    }
+                  : {
+                      justifyContent: "center",
+                    },
+              ]}
+            >
+              <ListItemIcon
                 onClick={() => navigate(`/articles`)}
+                sx={[
+                  {
+                    minWidth: 0,
+                    justifyContent: "center",
+                  },
+                  open
+                    ? {
+                        mr: 3,
+                      }
+                    : {
+                        mr: "auto",
+                      },
+                ]}
+              >
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText
+                onClick={() => navigate(`/articles`)}
+                primary="all posts"
+                sx={[
+                  open
+                    ? {
+                        opacity: 1,
+                      }
+                    : {
+                        opacity: 0,
+                      },
+                ]}
               />
             </ListItemButton>
-            <ListItemButton onClick={() => handleClick()} sx={{ pl: 0 }}>
-              <ListItemText primary="Topics" />
-              {open ? <ExpandLess /> : <ExpandMore />}
-            </ListItemButton>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-              <Divider sx={{ bgcolor: "white" }} />
-              {topicMessage ? (
-                <List component="div" disablePadding>
-                  <ListItemButton sx={{ pl: 2 }}>
-                    <ListItemText primary={topicMessage} />
-                  </ListItemButton>
-                </List>
-              ) : (
-                <></>
-              )}
+          </ListItem>
+        </List>
+        <List component="div" disablePadding>
+          <ListItemButton sx={{ pl: 1 }}>
+            <ListItemText primary={topicMessage} />
+          </ListItemButton>
+        </List>
 
-              {topicList ? (
-                topicList.topics.map((topic) => (
-                  <List component="div" disablePadding key={topic.slug}>
-                    <ListItemButton
-                      sx={{ pl: 1 }}
-                      key={topic.slug}
-                      onClick={() => navigate(`/topics/${topic.slug}`)}
-                    >
-                      <ListItemText primary={topic.slug} key={topic.slug} />
-                    </ListItemButton>
-                  </List>
-                ))
-              ) : (
-                <></>
-              )}
-            </Collapse>
-          </List>
-        </div>
-      </nav>
+        {topicList ? (
+          topicList.topics.map((topic) => (
+            <List component="div" disablePadding key={topic.slug}>
+              <ListItem
+                key={topic.slug}
+                disablePadding
+                sx={{ display: "block" }}
+              >
+                <ListItemButton
+                  sx={[
+                    {
+                      minHeight: 48,
+                      px: 2.5,
+                    },
+                    open
+                      ? {
+                          justifyContent: "initial",
+                        }
+                      : {
+                          justifyContent: "center",
+                        },
+                  ]}
+                >
+                  <ListItemIcon
+                    sx={[
+                      {
+                        minWidth: 0,
+                        justifyContent: "center",
+                      },
+                      open
+                        ? {
+                            mr: 3,
+                          }
+                        : {
+                            mr: "auto",
+                          },
+                    ]}
+                  >
+                    <InboxIcon />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={topic.slug}
+                    sx={[
+                      open
+                        ? {
+                            opacity: 1,
+                          }
+                        : {
+                            opacity: 0,
+                          },
+                    ]}
+                  />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          ))
+        ) : (
+          <></>
+        )}
+
+        <Divider />
+      </Drawer>
     </aside>
   );
 }
